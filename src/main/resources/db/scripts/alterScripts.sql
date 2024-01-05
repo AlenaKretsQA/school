c0cc20912be28a5d1d152a0d97e0b397815da6d1
AlenaKretsQA <149445084+AlenaKretsQA@users.noreply.github.com>
2024-01-05 08:02:08 -0800

    Delete src directory

--- src/main/resources/db/scripts/alterScripts.sql
+++
@@ -1,6 +0,0 @@
--- 5 alter table
-alter table teachers add column email varchar(255);
-update teachers set email = 'petr.petrova@email.com' where id = 2;
-alter table teachers drop column email;
-alter table students add column gender char(1);
-update students set gender = 'f' where id = 1;
