3c2cbd8c2d886d14251dee088b7e688ba7aa8540
AlenaKretsQA <149445084+AlenaKretsQA@users.noreply.github.com>
2024-01-09 07:34:46 -0800

    Delete src directory

--- src/main/java/com/solvd/laba/persistence/people/IPersonDAO.java
+++
@@ -1,7 +0,0 @@
-package com.solvd.laba.persistence.people;
-
-import com.solvd.laba.domain.people.Person;
-import com.solvd.laba.persistence.CommonDAO;
-
-public interface IPersonDAO<T> extends CommonDAO<Person> {
-}
